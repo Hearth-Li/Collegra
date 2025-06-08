28ad1685e37074271406c2e8d8a2fce129f790cf
Hearth Li <hayo7924@gmail.com>
2025-06-08 10:34:09 +0800

    Modify UTC in models.py to be compatible with python 3.9

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, UTC
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
@@ -9,8 +9,8 @@ class Note(db.Model):
     content = db.Column(db.Text, nullable=False)
     course = db.Column(db.String(100))
     tags = db.Column(db.String(200))  # 存储为逗号分隔的字符串
-    created_at = db.Column(db.DateTime, default=lambda: datetime.now(UTC))
-    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(UTC), onupdate=lambda: datetime.now(UTC))
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
 
     def get_tags(self):
         """将存储的标签字符串转换为列表"""
@@ -28,5 +28,5 @@ class Card(db.Model):
     course = db.Column(db.String(100))
     is_favorite = db.Column(db.Boolean, default=False)
     reviewed = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=lambda: datetime.now(UTC))
-    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(UTC), onupdate=lambda: datetime.now(UTC)) 
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc)) 
